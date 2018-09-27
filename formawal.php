60e1fb28326c60f783c074e07e6f991db0a8a445
faizazharr <43517978+faizazharr@users.noreply.github.com>
2018-09-27 23:01:04 +0700

    Update formawal.php

--- formawal.php
+++ formawal.php
@@ -1,5 +1,26 @@
-<?php
-
-
-
+<form method="POST" action="">
+	<table>
+		<tr>
+			<td>Username</td>
+			<td><input type="text" name="user"></td>
+		</tr>
+		<tr>
+			<td>Password</td>
+			<td><input type="text" name="password"></td>
+		</tr>
+		<input type="submit" name="login" value="masuk"><br>
+	</table>
+</form>
+<?php 
+if (isset($_POST['login'])) {
+	$user = array('faiz');
+	$pass = array('faiz');
+	$u = $_POST['user']; 
+	$p = $_POST['password'];
+	if (in_array($u, $user) && in_array($p, $pass)) {
+		exec('proses.php');
+	}else{
+		echo "maaf username atau password anda salah";
+	}
+}
 ?>
